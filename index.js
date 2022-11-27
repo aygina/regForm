d3c1cd2ea3890865a337e1286f825abe59d4055a
aygina <aygina.v@gmail.com>
2022-11-27 19:14:58 +0300

    perfect!

--- index.js
+++ index.js
@@ -1,78 +1,70 @@
-// Доработать форму регистрации из прошлых заданий, сделав для нее валидацию через JS.
 
 let errors = [];
 
+function checkAll() {
+    validateText();
+    validateEmail();
+    validatePass();
 
-// function check() {
-//     let userName = document.getElementById("userName").value;
-//     let surname = document.getElementById("surname").value;
-//     let email = document.getElementById("email").value;
-//     let password = document.getElementById("password").value;
-//     let passLength = document.getElementById("password").value.length;
-//     let passwordConf = document.getElementById("passwordConf").value;
+    document.getElementById('errorsInfo').style.color = 'black';
+    document.getElementById('errorsInfo').innerHTML = errors.join('!<br>');
+    
+    if (errors.length === 0) {
+        document.getElementById('errorsInfo').style.color = 'green';
+        document.getElementById('errorsInfo').innerHTML = 'Сохранено!';
+    }
 
-//     if (userName == '') {
-//         alert('Поле "Имя" не заполнено')
-//     } else if (surname == '') {
-//         alert('Поле "Фамилия" не заполнено')
-//     } else if (email == '') {
-//         alert('Поле "Email" не заполнено')
-//     } else if (passLength < 7) {
-//         alert('Пароль должен содержать больше 6 символов')
-//     } else if (passwordConf != password) {
-//         alert('Введенные пароли не совпадают')
-//     } else
-//         alert("Добро пожаловать!");
-// }
-
+    errors = [];
+}
 
 function checkValidity(input) {
     let validity = input.validity;
+
     if (validity.valueMissing) {
-        error.push('Поле ' + input.placeholder + ' не заполнено');
-    }
-    if (validity.patternMismatch) {
-        error.push('Неверный формат заполнения');
-    }
-    if (validity.rangeOverflow) {
-        let max = getAttribute(input, 'max');
-        error.push('Максимальное значение не может быть больше' + max);
+        errors.push('Поле ' + input.placeholder + ' не заполнено');
     }
 }
 
-function checkAll() {
-    errors = [];
-    let inputs = document.querySelectorAll('input');
+function validateText() {
 
+    let inputs = document.querySelectorAll('input[type=text]');
     for (let input of inputs) {
         checkValidity(input);
     }
-    document.getElementById('errorsInfo').innerHTML = errors.join('.<br>');
 }
+
 
 function validateEmail() {
     let email = document.getElementById("email").value;
     let mailFormat = /(\<|^)[\w\d._%+-]+@(?:[\w\d-]+\.)+(\w{2,})(\>|$)/i;
     if (email.match(mailFormat)) {
-        console.log(email);
+        return (email);
     } else {
-        console.log("Ваш адрес электронной почты введен неверно!");
+        errors.push("Ваш адрес электронной почты введен неверно");
     }
 }
 
 function validatePass() {
     let password = document.getElementById("password").value;
+    let passwordConf = document.getElementById("passwordConf").value;
+    let passLength = document.getElementById("password").value.length;
     let passFormat = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
-    if (password.match(passFormat)) {
-        console.log(password);
+
+    if (passLength < 7) {
+        errors.push('Пароль должен содержать больше 6 символов')
+    } else if (passwordConf !== password) {
+        errors.push('Введенные пароли не совпадают');
+    } else if (password.match(passFormat)) {
+        return (password);
     } else {
-        console.log("Слишком лёгкий пароль!");
+        errors.push("Слишком лёгкий пароль");
     }
 }
 
 function showPass() {
     let pass = document.getElementById("password");
     let passConf = document.getElementById("passwordConf");
+
 
     if (pass.type === "password") {
         pass.type = "text";
